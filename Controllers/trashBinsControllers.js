750a82578272833751f1f0442ae73433b267a2f3
Yanay Sova <121397386+yanaysova@users.noreply.github.com>
2023-06-01 10:48:57 +0300

    fixes

--- Controllers/trashBinsControllers.js
+++ Controllers/trashBinsControllers.js
@@ -3,14 +3,18 @@ const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
 const getBins = catchAsync(async (req, res, next) => {
-  const path = req.file.path;
+  // const path = req.file.path;
+  // console.log(path);
   const { cords } = req.body;
-  console.log(JSON.parse(cords));
-  console.log(path);
-  const userLatitude = Number(cords.lat.toFixed(6));
-  const userLongitude = Number(cords.lng.toFixed(6));
+  const parsedCords = JSON.parse(cords);
+  const userLatitude = Number(parsedCords.lat.toFixed(6));
+  const userLongitude = Number(parsedCords.lng.toFixed(6));
   //Add axios post request to data science model.
   const binType = req.body.type;
+  console.log(binType);
+  console.log(`Latitude: ${userLatitude}`);
+  console.log(typeof userLatitude);
+  console.log(`Longitude: ${userLongitude}`);
   const results = await trashBin
     .find({
       type: binType,
@@ -20,13 +24,13 @@ const getBins = catchAsync(async (req, res, next) => {
             type: "Point",
             coordinates: [userLongitude, userLatitude],
           },
-          $maxDistance: 1000,
+          $maxDistance: 5000,
         },
       },
     })
     .limit(3);
-  if (results === null) {
-    return nextnext(new AppError("No matching trash bin found.", 404));
+  if (results.length === 0) {
+    return next(new AppError("No matching trash bin found.", 404));
   }
   res.status(200).json({
     status: "success",
